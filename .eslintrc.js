f4c278e35b12a1324bb24f5774dac8f835d3a341
Oleksandr Kryvchenko <amzkryvchenko@gmail.com>
2023-02-15 20:42:32 +0100

    readme updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
   extends: "eslint:recommended",
   overrides: [],
   parserOptions: {
     ecmaVersion: "latest",
-    sourceType: "module"
+    sourceType: "module",
   },
   rules: {
-    modules: true
-  }
+    modules: true,
+  },
 };
